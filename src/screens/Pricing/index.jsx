0c5143bc7b3488118b8947fc0d0592b5f5cd41ba
Ndigithe-john <ndigithejohn@gmail.com>
2025-02-12 16:28:31 +0300

    module importation

--- src/screens/Pricing/index.jsx
+++ src/screens/Pricing/index.jsx
@@ -1,6 +1,6 @@
 // Uses the same styles as Product
 import PageNav from "@components/Header";
-import styles from "../Product/Product.Module.css";
+import styles from "../Product/Product.module.css";
 
 export default function Pricing() {
   return (
